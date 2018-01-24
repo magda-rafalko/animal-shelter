75837691716dbc9466bf114306bef7e75be7c142
Magda Rafałko <magda.filipkowskaa@gmail.com>
2018-01-24 19:07:12 +0100

    Dodanie komunikacji z REST API przy pomocy biblioteki Axios
    - pobieranie wszystkich danych
    - dodawanie nowego psa

--- dogs.js
+++ dogs.js
@@ -1,76 +1,119 @@
-let editMode = false;
-let editRowNumber = 0;
-let editTable = document.getElementById("table");
-let changeButton = document.getElementById("addDog");
-let legend = document.querySelector('legend');
-let tbody = table.querySelector('tbody');
+function getAxios() {axios.get('EXAMPLE_URL')
+    .then(function(response) {
+        const obj = response.data;
+            for ( let i = 0; i<obj.length; i++) {
+                insertDog(obj[i]);
+            }
+    }
+
+    )
+    .catch(error => console.log(error))
+    }
+
+
+function insertDog(dog) {
+
+    const table = document.getElementById('table').querySelector('tbody');
+    const newRow = table.insertRow(table.rows.length);
+
+    newRow.insertCell(0).innerHTML = dog.name;
+    newRow.insertCell(1).innerHTML = dog.age;
+    newRow.insertCell(2).innerHTML = dog.size;
+    newRow.insertCell(3).innerHTML = dog.sex;
+    newRow.insertCell(4).innerHTML = dog.description;
+    newRow.insertCell(5).innerHTML = dog.dateInserted;
+    newRow.insertCell(6).innerHTML = dog.keeperEmail;
+    const editButton = document.createElement('button');
+    editButton.setAttribute('id', 'editButton');
+    editButton.setAttribute('class', 'smallButton');
+    editButton.setAttribute('onclick', 'startEditRow(this)');
+    editButton.innerHTML = '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>';
+    newRow.insertCell(7).appendChild(editButton);
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute( 'class', 'smallButton');
+    deleteButton.innerHTML = `<i class="fa fa-trash icon" aria-hidden="true"></i>`;
+    editButton.setAttribute('onclick', 'deteteRow(this)');
+    newRow.insertCell(8).appendChild(deleteButton);
+}
+
+const editMode = false;
+const editRowNumber = 0;
+const editTable = document.getElementById("table");
+const changeButton = document.getElementById("addDog");
+const legend = document.querySelector('legend');
+const tbody = table.querySelector('tbody');
 
 
 document.getElementById('addDate').valueAsDate = new Date();
 
 function addOrEditDog() {
   if (editMode)
-    editDog()
+    editDog();
   else
     checkbox()
 }
 
 function addDog() {
 
-  let table = document.getElementById('table').querySelector('tbody');
-  let newRow = table.insertRow(table.rows.length);
-
-  let cell1 = newRow.insertCell(0);
-  let name = document.getElementById('dogName').value;
-  cell1.innerHTML = name;
-
-  let cell2 = newRow.insertCell(1);
-  let age = document.getElementById('dogAge').value;
-  cell2.innerHTML = age;
-
-  let cell3 = newRow.insertCell(2);
-  let size = document.querySelector('input[name="dogSize"]:checked').value;
-  cell3.innerHTML = size;
-
-  let cell4 = newRow.insertCell(3);
-  let sex = document.querySelector('input[name="dogSex"]:checked').value;
-  cell4.innerHTML = sex;
-
-  let cell5 = newRow.insertCell(4);
-  let subscribe = document.getElementById('dogSubscribe').value;
-  cell5.innerHTML = subscribe;
-
-  let cell6 = newRow.insertCell(5);
-  let date = document.getElementById('addDate').value;
-  cell6.innerHTML = date;
-
-  let cell7 = newRow.insertCell(6);
-  let email = document.getElementById('addEmail').value;
-  cell7.innerHTML = email;
-
-  let cell8 = newRow.insertCell(7);
-  let btn2 = document.createElement('input');
-  btn2.type = "button";
-  btn2.className = "smallButton";
-  btn2.value = "Edytuj";
-  btn2.id = "editButton";
-  btn2.onclick = function() {startEditRow(this)};
-  cell8.appendChild(btn2);
-
-  let cell9 = newRow.insertCell(8);
-  let btn = document.createElement('button');
-  btn.className = "remove-button";
-  btn.innerHTML = "[X]";
-  cell9.appendChild(btn);
+  const dog = {
+    age: parseAge(),
+    dateInserted: document.getElementById('addDate').value,
+    description: document.getElementById('dogSubscribe').value,
+    keeperEmail: document.getElementById('addEmail').value,
+    name: document.getElementById('dogName').value,
+    sex: parseSex(),
+    size: parseSize(),
+}
+    axios.post('EXAMPLE_URL', dog)
+        .then(function(response) {
+            console.log(response.data.id);
+            insertDog(dog);
+        })
+        .catch(error => console.log(error));
 }
 
-tbody.onclick = function(e) {
-  if (e.target.className != "remove-button") return;
+// funkcja na sex i size i age
 
-  let i = e.target.closest('tr');
+function parseSex() {
+    if (document.querySelector('input[name="dogSex"]:checked').value === "Samiec") {
+        return 'MALE';
+    }
+    return 'FEMALE';
+}
+function parseSize() {
+    if (document.querySelector('input[name="dogSize"]:checked').value === "Mały") {
+        return 'SMALL';
+    } else if (document.querySelector('input[name="dogSize"]:checked').value === "Średni") {
+        return 'MEDIUM';
+    } else {
+    return 'LARGE';
+}}
+function parseAge() {
+    if (document.getElementById('dogAge').value === "brak informacji") {
+        return '-';
+    } else if (document.getElementById('dogAge').value === "Więcej niż 10"){
+        return '>10';
+    } return document.getElementById('dogAge').value;
+    }
+
+// funkcja usuwająca wiersz
+
+function deleteRow(row) {
+    let i = row.parentNode.parentNode.rowIndex;
+    document.getElementById('table').deleteRow(i);
+}
+
+
+// zmienić na takie jak edycja
+tbody.onclick = function(e) {
+  if (e.target.className !== "remove-button") return;
+
+  const i = e.target.closest('tr');
   console.log(i);
   i.remove();
 }
+
+// funkcja zaczynająca edycję
 
 function startEditRow(row) {
  editMode = true;
@@ -80,17 +123,19 @@ function startEditRow(row) {
 
       document.getElementById("dogName").value = editTable.rows[editRowNumber].cells[0].innerHTML;
       document.getElementById("dogAge").value = editTable.rows[editRowNumber].cells[1].innerHTML;
-      let checkedValSize = editTable.rows[editRowNumber].cells[2].innerHTML;
-      let checkedButSize = document.querySelector(`input[value=${checkedValSize}]`);
+      const checkedValSize = editTable.rows[editRowNumber].cells[2].innerHTML;
+      const checkedButSize = document.querySelector(`input[value=${checkedValSize}]`);
       checkedButSize.checked = true;
-      let checkedValSex = editTable.rows[editRowNumber].cells[3].innerHTML;
-      let checkedButSex = document.querySelector(`input[value=${checkedValSex}]`);
+      const checkedValSex = editTable.rows[editRowNumber].cells[3].innerHTML;
+      const checkedButSex = document.querySelector(`input[value=${checkedValSex}]`);
       checkedButSex.checked = true;
       document.querySelector('input[name="dogSex"]:checked').value = editTable.rows[editRowNumber].cells[3].innerHTML;
       document.getElementById("dogSubscribe").value = editTable.rows[editRowNumber].cells[4].innerHTML;
       document.getElementById("addDate").value = editTable.rows[editRowNumber].cells[5].innerHTML;
       editTable.rows[editRowNumber].style.color = "red";
 }
+
+// funkcja edycja psa
 
 function editDog() {
     editTable.rows[editRowNumber].cells[0].innerHTML = document.getElementById("dogName").value;
@@ -105,20 +150,24 @@ function editDog() {
     legend.textContent = "Dodaj nowego psa:";
 }
 
+// alert 1 : zły mail
+
 function showAlert1() {
-  let div = document.createElement('div');
+  const div = document.createElement('div');
   div.className = "mailAlert";
   div.innerHTML = "<strong>Ups!</strong> Sprawdź czy mail jest prawidłowy.";
   myForm.before(div);
-  setTimeout( () => div.remove(), 2000);
+  // setTimeout( () => div.remove(), 2000);
 }
 
+// alert 2 : niezaakceptowane
+
 function showAlert2() {
-  let div = document.createElement('div');
+  const div = document.createElement('div');
   div.className = "addAlert";
   div.innerHTML = "<strong>Ups!</strong> Musisz zaakceptować dodanie psa.";
   myForm.before(div);
-  setTimeout( () => div.remove(), 2000);
+  // setTimeout( () => div.remove(), 2000);
 }
 
 function checkbox() {
@@ -137,14 +186,14 @@ function checkbox() {
   }
 }
 
-// podświetlanie komórek nad którymi jeździmy myszą
+// podświetlanie komórek nad którymi jeździmy myszą (zmienić na podświetlenie rowa)
 
 let selectedTh;
 
 editTable.onmouseover = function(event) {
   let target = event.target;
-    while (target != this) {
-      if (target.tagName == 'TD' || target.tagName == 'TH' ) {
+    while (target !== this) {
+      if (target.tagName === 'TR' && target.id !== 'head') {
         highlight(target);
         return;
       }
@@ -161,16 +210,17 @@ function highlight(node) {
 }
 
 //sortowanie po kliknieciu nazwy wiesza
+
 table.onclick = function (event) {
-  if (event.target.tagName != 'TH') return;
+  if (event.target.tagName !== 'TH') return;
 
   let th = event.target;
   sortTable(th.cellIndex, th.dataset.type);
 };
 
 function sortTable(colNum, type) {
-  let tbody = table.querySelector('tbody');
-  let rowsArray = Array.from(tbody.rows);
+  const tbody = table.querySelector('tbody');
+  const rowsArray = Array.from(tbody.rows);
   let compare;
 
   switch (type) {
